3621d0b1a66006985457968e4cb37bad33b2c63a
raymeloc <raymeloc@gmail.com>
2025-10-03 22:09:44 -0300

    chore: yarn version

--- src/app/(public)/home/page.tsx
+++ src/app/(public)/home/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { WhatsappButton } from "@/layout/public/whatsapp-button";
 import { Suspense } from "react";
-import { Banners } from "../components/banners";
-import { Brands } from "../components/brands";
-import { InfoBar } from "../components/info-bar";
+import { Banners } from "./components/banners";
+import { Brands } from "./components/brands";
+import { InfoBar } from "./components/info-bar";
 import { useProduct } from "@/hooks/useProduct";
-import { Offers } from "../components/offers";
+import { Offers } from "./components/offers";
 
 export default function HomePage() {
   const { products } = useProduct();
